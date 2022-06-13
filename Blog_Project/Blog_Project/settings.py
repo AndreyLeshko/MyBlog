f8a55bc7fdc9e428bf52d77defbc1bf8a01aca1d
AndreyLeshko <andreyleshko2001@gmail.com>
2022-06-13 13:40:05 +0700

    add STATIC_ROOT in settings

--- Blog_Project/Blog_Project/settings.py
+++ Blog_Project/Blog_Project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-angfog8!5r0ou&odnlyh^!2&$h&e+=$^8s9-n=1d7tzu#pvm)!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
